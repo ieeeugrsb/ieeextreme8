3a295f93522075bce777d9056d0eac8b159ddc58
Rafael Bailón <rafaelbailon@ieee.org>
2014-10-18 02:33:06 +0200

    [01] Read input

--- 01_IEEE Electronic Devices Society/01.py
+++ 01_IEEE Electronic Devices Society/01.py
@@ -5,3 +5,11 @@ Created on Sat Oct 18 02:24:38 2014
 @author: rafael
 """
 
+first_line = raw_input().split()
+
+seq_len = int(first_line[0])
+subseq_len = int(first_line[1])
+index = int(first_line[2])
+
+sequence = raw_input().split()
+
